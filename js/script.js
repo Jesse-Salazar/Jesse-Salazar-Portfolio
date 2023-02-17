29caa743330e811b090e0e0c7d7337ddee7fff17
Jesse Salazar <jsn4991@yahoo.com>
2023-02-16 20:06:47 -0600

    switch out resumes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+//Smooth Nav Scroll
 const scrolly = document.querySelectorAll('a[href^="#"]');
 
 scrolly.forEach(anchor => {
@@ -10,6 +10,7 @@ scrolly.forEach(anchor => {
     });
 });
 
+//Mobile Nav
 const btn = document.getElementById('menu-btn');
 const nav = document.getElementById('menu');
 
@@ -20,8 +21,8 @@ btn.addEventListener('click', () => {
 });
 
 //Collapsing Resume 
-var coll = document.getElementsByClassName("collapsible");
-var i;
+let coll = document.getElementsByClassName("collapsible");
+let i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
@@ -37,42 +38,4 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
-//Message after submit is pressed
 
-// Get the form element
-// const form = document.getElementById("form");
-
-// // Get the message element where you want to display the success message
-// const message = document.getElementById("formOutputBox");
-
-// // Listen for the form's submit event
-// form.addEventListener("submit", event => {
-//     // Prevent the default form behavior (navigating away from the page)
-//     event.preventDefault();
-
-//     // Create a new FormData object
-//     const formData = new FormData(form);
-
-//     // Send the form data to the server using the fetch API
-//     fetch("https://docs.google.com/forms/u/0/d/e/1FAIpQLScxPp6p-LOvhPC6FxvA9D6VEJitcTvkrAMu5wMOkoEZnhSWfw/formResponse", {
-//         mode: 'no-cors',
-//         method: "POST",
-//         body: formData
-//     })
-//     .then(response => {
-//         if (response.ok) {
-//             // If the form was sent successfully, display a success message
-//             message.innerHTML = "Form sent successfully";
-//             message.style.color = "green";
-//         } else {
-//             // If the form was not sent successfully, display an error message
-//             message.innerHTML = "Error sending form";
-//             message.style.color = "red";
-//         }
-//     })
-//     .catch(error => {
-//         console.error("Error sending form:", error);
-//         message.innerHTML = "Error sending form";
-//         message.style.color = "red";
-//     });
-// });
